796b63f2d5a88a8e7e047242d42f0b912d913a37
Carlos Roberto da Silva Filho <silva@edu.sc.senai.br>
2026-06-14 08:44:08 -0300

    front end do usuário

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 
-const db = new Sequelize('ecom','root','senai',{
+const db = new Sequelize('ecom','root','root',{
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
